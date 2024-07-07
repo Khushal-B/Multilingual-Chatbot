5c1a27b753dbb862221d000fb96cf5a5f95d37e5
KhushalBhalia <160427018+Khushal-B@users.noreply.github.com>
2024-07-07 19:55:25 +0530

    Update firebase.js
    Removed API keys 😅

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDJde2AirREXf8oCftdbcsftZa-El6aybc",
-  authDomain: "sale-buddy-fbaf7.firebaseapp.com",
+  apiKey: "API_KEY_HERE",
+  authDomain: "PUT_DOMAIN_HERE",
   projectId: "sale-buddy-fbaf7",
   storageBucket: "sale-buddy-fbaf7.appspot.com",
   messagingSenderId: "720829636966",
-  appId: "1:720829636966:web:20840163029553c6c828a7",
+  appId: "PUT_ID_HERE",
   measurementId: "G-EMQ8GE5V8H"
 };
 
